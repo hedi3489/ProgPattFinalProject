c149b37f162f0df013657d19b0b67668127505f4
Conan <98342638+ZYMNZ@users.noreply.github.com>
2023-05-03 20:34:52 -0400

    Create Book.cs

---
+++ Classes/Book.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsReserved { get; set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            IsReserved = false;
+        }
+    }
+}
